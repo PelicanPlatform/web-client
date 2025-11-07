1731ab125ba1d024b9fd680dd84072f4b37638fe
clock <clock@wisc.edu>
2025-11-07 15:15:59 -0600

    Update things to work for the website
    - Update pnpm workspace
    - Update website package imports
    - Fixup package exports

--- website/next.config.js
+++ website/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     },
-    transpilePackages: ["next-image-export-optimizer"],
+    transpilePackages: ["next-image-export-optimizer", "@pelicanplatform/components", "@pelicanplatform/web-client"],
     env: {
         nextImageExportOptimizer_imageFolderPath: "public/images",
         nextImageExportOptimizer_exportFolderPath: "out",
